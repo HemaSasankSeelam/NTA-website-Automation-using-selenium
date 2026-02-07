23febd0679f1e027e0480e77da50d1f520e41ef1
HEMA SASANK. SEELAM <149160505+HemaSasankSeelam@users.noreply.github.com>
2026-02-07 08:37:38 +0530

    selenium automation

---
+++ response_sheets/1-jee_response.py
@@ -0,0 +1,155 @@
+import selenium
+
+from selenium.common import exceptions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+import pandas as pd
+from pathlib import Path
+import traceback 
+import time
+
+class JEE_RESPONSE_SHEETS:
+
+    def __init__(self, url, excel_path):
+        self.url = url # website url
+        self.excel_path = excel_path # credentials excel path
+        self.parent_path = Path(excel_path).parent # parent of the file
+        self.file_name = Path(excel_path).stem # file name without extension
+        self.results_saving_path = self.parent_path / Path(str(self.file_name) + "_results_by_selenium.xlsx") # excel path for the response sheet links
+
+        chrome_service = Service(executable_path=ChromeDriverManager().install()) # installing the chrome driver
+        self.driver = webdriver.Chrome(service=chrome_service) # driver 
+        self.driver.set_window_position(x=200, y=50) # sets the window position
+        self.webdriver_wait = WebDriverWait(driver=self.driver, timeout=10) # webdriver wait
+
+    def read_from_excel(self) -> None:
+        self.dataframe = pd.read_excel(self.excel_path) # reads the credential excel
+        self.dataframe = self.dataframe.drop_duplicates(subset=['APPLICATION_NO']) # removes the duplicates from the application column
+        self.dataframe.to_excel(self.excel_path, index=False) # and save the excel path
+        self.dataframe = pd.read_excel(self.excel_path)
+
+        self.filtered_dataframe = self.dataframe.loc[self.dataframe["STATUS"] == "NO"] # filters the dataframe whose status is NO
+        if len(self.filtered_dataframe) == 0:
+            print("ALL MEMBERS STATUS IS YES".center(50, "*"))
+            return
+        
+        applications_nos = self.filtered_dataframe['APPLICATION_NO']
+
+        for application_no in applications_nos.tolist():
+
+            try:
+                password = self.filtered_dataframe.loc[self.filtered_dataframe["APPLICATION_NO"] == application_no]["PASSWORD"].values[0]
+
+                is_verified = self.main_page(application_no=application_no, password=password)
+
+                if is_verified:
+                    # if the application is verified then update the status to YES
+                    self.dataframe.loc[self.dataframe["APPLICATION_NO"] == application_no, ["STATUS"]] = "YES"
+                    self.dataframe.to_excel(self.excel_path, index=False) # update the excel file 
+
+            except Exception as e:
+                # if any error handling the error
+                print(f"Error for application {application_no}: {e}")
+
+        print("COMPLETED".center(50, "*"))
+
+    def main_page(self, application_no, password) -> bool:
+
+        self.driver.get(self.url) # gets the website url
+
+        try:
+            application_no_field = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txtRegno")
+            application_no_field.send_keys(application_no) # sends application no
+
+            password_field = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txtPassword")
+            password_field.send_keys(password) # sends the password
+
+            captcha_field = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txtsecpin")
+            captcha_field.send_keys() # clicks on the captcha column
+
+            
+            list_of_xpaths = ["//a[@id='ctl00_LoginContent_linkDownConfirm']", "//input[@id='ctl00_ContentPlaceHolder1_btnYes']"]
+            # if any one is visible then skips the next step
+            # the first one is for the inside page for downloading the response sheet link
+            # the second one is for the duplicate application login handling
+            self.webdriver_wait.until(EC.visibility_of_any_elements_located((By.XPATH, "|".join(list_of_xpaths))))
+
+            
+            try:
+                duplicate_login= self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_btnYes")
+                if duplicate_login.is_displayed():
+                    # if the duplicate login detected then we click on the logout and again enter password and enter's captcha
+                    duplicate_login.click()
+
+                    time.sleep(3)
+                    self.driver.implicitly_wait(time_to_wait=3) # manually wait for 3 sec
+                    
+
+                    return self.main_page(application_no, password)
+            
+            except Exception as e:
+                pass
+                
+            
+        except exceptions.TimeoutException:
+            print("Login Failed")
+            return False
+
+        else:
+            self.inside_page(application_no, password)
+            return True
+
+    def inside_page(self, application_no, password) -> None:
+        try:
+            # getting the response sheet link
+            url_link = self.driver.find_element(By.ID, "ctl00_LoginContent_rptViewQuestionPaper_ctl01_lnkviewKey").get_attribute("href")
+
+            data_dict = {"APPLICATION_NO": application_no, "PASSWORD": password, "URL": url_link, "STATUS": "NO"}
+            self.write_to_excel_file(data_dict)
+
+        except Exception as e:
+            print(f"Error inside page: {e}")
+            # if any error occurs then we will save the data with URL as AB
+            data_dict = {"APPLICATION_NO": application_no, "PASSWORD": password, "URL": "AB", "STATUS": "NO"}
+            self.write_to_excel_file(data_dict)
+        
+        finally:
+
+            self.driver.implicitly_wait(time_to_wait=3) # manually wait for 3 sec
+
+            self.driver.find_element(By.LINK_TEXT, "Logout").click() # clicks on the logout button in the main page
+            self.driver.find_element(By.ID, "btnLogout").click() # clicks on the logout button in the previous page
+
+    def write_to_excel_file(self, data_dict: dict) -> None:
+        try:
+            if not self.results_saving_path.exists():
+                # this is for the first time creation of the file
+                pd.DataFrame([data_dict]).to_excel(self.results_saving_path, index=False)
+                return
+            
+            #columns = ["APPLICATION_NO", "PASSWORD", "URL", "STATUS"]
+            df_existing = pd.read_excel(self.results_saving_path)
+            df_new = pd.DataFrame([data_dict])
+            # combined the two dataframes existing then new dataframe
+            df_combined = pd.concat([df_existing, df_new], ignore_index=True)
+            df_combined.to_excel(self.results_saving_path, index=False)
+
+        except Exception as e:
+            print(f"Exception in write_to_excel_file: {e}")
+            traceback.print_exc()
+            exit()
+
+if __name__ == "__main__":
+    url = "https://examinationservices.nic.in/JeeMainx2026/Root/CandidateLogin.aspx?enc=Ei4cajBkK1gZSfgr53ImFVj34FesvYg1WX45sPjGXBqfcvMYv/FHq/Da9QEnq781"
+    excel_path = r"F:\2026_jee_response_sheets\credentials.xlsx"
+
+    # columns = [APPLICATION_NO, PASSWORD, STATUS]
+    jee_object = JEE_RESPONSE_SHEETS(url=url, excel_path=excel_path)
+    jee_object.read_from_excel()
---
+++ response_sheets/wrong_answers.py
@@ -0,0 +1,144 @@
+import selenium
+
+from selenium.common import exceptions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+import pandas as pd
+from pathlib import Path
+import traceback 
+import time
+import pyhtml2pdf.converter
+import pdfkit
+
+class WRONG_ANSWERS:
+
+    def __init__(self):
+
+        chrome_service = Service(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=chrome_service)
+        self.webdriver_wait = WebDriverWait(driver=self.driver, timeout=7)
+
+    def take_screen_shot(self,student_excel_path):
+
+        self.student_excel_path = Path(student_excel_path)
+        self.student_df = pd.read_excel(student_excel_path)
+
+        application_no = self.student_excel_path.stem # get the application no from the file name
+        base_folder = self.student_excel_path.parent.parent # goes back 2 back parent
+
+        if (base_folder / f"{application_no}.pdf").exists():
+            # means already pdf generated
+            return
+
+        self.images_folder = self.student_excel_path.parent / "wrong_questions"
+        self.images_folder.mkdir(parents=True, exist_ok=True)
+
+        filtered_df = self.student_df.loc[:,["APPLICATION_NO","TEST_DATE","TEST_TIME","URL","QUESTION_NO","QUESTION_IDS","ANSWERED_TYPE","MARKS"]]
+
+        marks_filtered_df = filtered_df.loc[filtered_df["MARKS"] != 4] # filters the dataframe whose marks is not 4
+
+        application_no = filtered_df.loc[0,"APPLICATION_NO"]
+        test_date = filtered_df.loc[0,"TEST_DATE"]
+        test_time = filtered_df.loc[0,"TEST_TIME"]
+
+        question_nos_based_on_marks = marks_filtered_df["QUESTION_NO"].to_list() # gets the question nos whose marks is not 4
+
+        question_nos_based_on_answered_type = filtered_df.loc[filtered_df["ANSWERED_TYPE"] == "mcq_answered_marked_for_review",["QUESTION_NO"]]\
+                                                         .values.flatten().tolist() # gets the question nos whose answered type is marked for review
+         
+        sorted_question_ids = sorted(list(set(question_nos_based_on_marks + question_nos_based_on_answered_type)))
+        
+        question_ids = filtered_df.loc[filtered_df["QUESTION_NO"].isin(sorted_question_ids),["QUESTION_IDS"]].values.flatten().tolist() # gets the question ids whose question nos is in the sorted question nos
+
+        url = filtered_df["URL"].iloc[0]
+
+        self.driver.get(url=url)
+        self.driver.maximize_window()
+    
+        time.sleep(3)
+        application_heading_img = self.driver.find_element(by=By.XPATH, value='//div[@class="main-info-pnl"]')
+        application_heading_img.screenshot((self.images_folder/"00.png").as_posix())
+
+        html_code = f"""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>{application_no}</title>
+        </head>
+        <body>
+        <div class="head" style="display: flex; justify-content:space-evenly; background-color: rgb(113, 183, 211);">
+            <h2>Test Date: {test_date}</h2>
+            <h2>Application No: {application_no}</h2>
+            <h2>Test Time: {test_time}</h2>
+        </div>
+        <br>
+        <img src="./00.png" alt="Unable to display main heading" style='display:block; border-bottom:2px solid black;'\n>
+        <br>
+        <hr>
+        """
+        maths_entered = False
+        physics_entered = False
+        chemistry_entered = False
+
+        for question_id in question_ids:
+
+            question_no = int(filtered_df.loc[filtered_df["QUESTION_IDS"] == question_id,["QUESTION_NO"]].values[0][0])
+
+            question_image = self.driver.find_elements(by=By.XPATH, value="//table[@class='questionPnlTbl']")[question_no-1]
+            question_image_path = self.images_folder / f"{question_no:02d}.png"
+            question_image.screenshot(question_image_path.as_posix())
+
+            if question_no <= 25 and not maths_entered:
+                maths_entered = True
+                html_code += "\n<hr><br><h2 style='text-align:center;'>MATHEMATICS</h2><br>"
+            elif 25 < question_no <= 50 and not physics_entered:
+                physics_entered = True
+                html_code += "\n<br><h2 style='text-align:center;' class='break'>PHYSICS</h2><hr><br>"
+            elif 50 < question_no <= 75 and not chemistry_entered:
+                chemistry_entered = True
+                html_code += "\n<br><h2 style='text-align:center;' class='break'>CHEMISTRY</h2><hr><br>"
+
+            html_code += f"\n<img src='./{question_image_path.name}' alt='Unable to display question no {question_image_path.stem}' style='display:block; border-bottom:2px solid black;'>\n"
+        
+        html_code += """
+        <style>
+            hr {
+                border: 2px dashed pink;
+            }
+            @media print
+            {
+                .break
+                {
+                    page-break-before: always;
+                }
+            }
+        </style>
+        </body>
+        </html>
+        """
+        html_file_path = (self.images_folder/"index.html").as_posix()
+        with open(html_file_path,'w') as fo:
+            fo.write(html_code)
+
+        pdf_path = (self.images_folder.parent.parent/f"{application_no}.pdf").as_posix()
+        # pyhtml2pdf.converter.convert(source=html_file_path, target=pdf_path,
+        #                              print_options={"paperWidth":11})
+        
+        # convert the html to pdf using pdfkit module
+        pdfkit.from_file(input = html_file_path,
+                        output_path = pdf_path,
+                        options={'enable-local-file-access': '',
+                                'page-size': 'A4'})
+
+        
+if __name__ == "__main__":
+    pass
+    # WRONG_ANSWERS(r"F:\response_sheets\22-01-2025\phase-1\application_no\application_no.xlsx")
